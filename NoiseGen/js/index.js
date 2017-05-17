1d7a142fa79dae7febd9fd90a76150ebdf81bf57
CHRIS-PC\Chris <christian.gnaw@gmail.com>
2017-05-17 12:22:44 -0400

    Added Initial Commit

---
+++ NoiseGen/js/index.js
@@ -0,0 +1,99 @@
+
+var camera, scene, renderer, geometry, material, mesh, noise;
+
+var worldWidth = 256, worldDepth = 256,
+    worldHalfWidth = worldWidth / 2, worldHalfDepth = worldDepth / 2;
+
+init();
+animate();
+
+
+function generateHeight(width, height) {
+
+    var size = width * height, data = new Uint8Array(size), quality = 1, z = Math.random() * 100;
+
+    for (var j = 0; j < 5; j++) {
+
+        for (var i = 0; i < size; i++) {
+
+            var x = i % width, y = ~~(i / width);
+            data[i] += Math.abs(improvedPerlinNoise(x / quality, y / quality, z) * quality * 1.75);
+
+        }
+
+        quality *= 5;
+
+    }
+
+    return data;
+
+}
+
+function init() {
+    var data = generateHeight(worldWidth, worldDepth);
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 100000);
+    //camera.position.y = data[worldHalfWidth + worldHalfDepth * worldWidth] * 10 + 500;
+    camera.position.y = 5000;
+    scene.add(camera);
+
+    var light = new THREE.AmbientLight(0x404040);
+    scene.add(light);
+
+    geometry = new THREE.CubeGeometry(200, 200, 200);
+    material = new THREE.MeshNormalMaterial();
+    material.wireframe = true;
+
+    mesh = new THREE.Mesh(geometry, material);
+    //scene.add(mesh);
+
+    var terrain = new THREE.PlaneBufferGeometry(7500, 7500, worldWidth - 1, worldDepth - 1);
+    terrain.rotateX(- Math.PI / 2);
+
+
+    //var terrain = new THREE.PlaneBufferGeometry(100, 100, 100);
+    var vertices = terrain.attributes.position.array;
+
+
+    for (var i = 0, j = 0; i < vertices.length; i++ , j += 3) {
+        vertices[j + 1] = data[i] * 10;
+    }
+
+    noise = new THREE.Mesh(terrain, material);
+
+    scene.add(noise);
+    camera.lookAt(noise);
+
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    render();
+
+}
+
+function render() {
+
+    // *** Update the scene ***
+
+    // Set the camera to always point to the centre of our scene, i.e. at vector 0, 0, 0
+    camera.lookAt(scene.position);
+
+    // Move the camera in a circle with the pivot point in the centre of this circle...
+    // ...so that the pivot point, and focus of the camera is on the centre of our scene.
+    var timer = new Date().getTime() * 0.00005;
+
+    camera.position.x = Math.floor(Math.cos(timer) * 7500);
+    camera.position.z = Math.floor(Math.sin(timer) * 7500);
+
+
+    renderer.render(scene, camera);
+}
