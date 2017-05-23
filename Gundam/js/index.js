953f50b8726c1a8764dbfc25186d6212fc1e0483
CHRIS-PC\Chris <christian.gnaw@gmail.com>
2017-05-23 13:27:45 -0400

    fixed some stupid rotation stuff

--- Gundam/js/index.js
+++ Gundam/js/index.js
@@ -56,10 +56,10 @@ function render() {
 
     // Move the camera in a circle with the pivot point in the centre of this circle...
     // ...so that the pivot point, and focus of the camera is on the centre of our scene.
-    timer = new Date().getTime() * 0.0005;
+    timer = new Date().getTime() * 0.00005;
 
-    camera.rotation.x = Math.cos(timer) * 7500;
-    camera.rotation.z = Math.sin(timer) * 7500;
+    camera.position.x = Math.floor(Math.cos(timer) * 7500);
+    camera.position.z = Math.floor(Math.sin(timer) * 7500);
 
     renderer.render(scene, camera);
 }
