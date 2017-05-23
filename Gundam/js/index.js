1e1a1a1a9be8f925c2412b448f97f30dc653f85f
CHRIS-PC\Chris <christian.gnaw@gmail.com>
2017-05-23 16:04:28 -0400

    no message

--- Gundam/js/index.js
+++ Gundam/js/index.js
@@ -10,7 +10,7 @@ function init() {
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 100000);
-    camera.y = 7500;
+    camera.y = 10000;
     //camera.position.y = data[worldHalfWidth + worldHalfDepth * worldWidth] * 10 + 500;
     scene.add(camera);
 
