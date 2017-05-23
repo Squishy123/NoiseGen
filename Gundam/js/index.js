67f627243003c1d310309e2b88ec921dd7112b97
CHRIS-PC\Chris <christian.gnaw@gmail.com>
2017-05-23 13:12:29 -0400

    scaled up

--- Gundam/js/index.js
+++ Gundam/js/index.js
@@ -26,7 +26,7 @@ function init() {
     var loader = new THREE.JSONLoader();
     loader.load('gundam.json', function(geometry) {
         mesh = new THREE.Mesh(geometry, material);
-        mesh.scale.set(1, 1, 1);
+        mesh.scale.set(10, 10, 10);
         mesh.position.set(0,0,0);
         scene.add(mesh);
     });
